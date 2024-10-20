0eb61d493cbb1d811435d990bb6375ab3a4de4be
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-10-20 14:28:03 +0200

    ejercicio 5  acabado

--- Basic/27-exercices-console-methods.js
+++ Basic/27-exercices-console-methods.js
@@ -1,5 +1,7 @@
 // 1. Crea un función que utilice error correctamente
-let edadParticipante1 = 1
+// 2. Crea una función que utilice warn correctamente
+// 3. Crea una función que utilice info correctamente
+let edadParticipante1 = 18
 
 class edadIlegal extends Error {
   constructor(message, edad) {
@@ -12,31 +14,46 @@ function comprobarEdadTorneo(edad) {
   if (edad > 18) {
     console.log("El participante es mayor de edad y puede concursar")
   } else if (edad === 18) {
-    console.log(
+    console.warn(
       "El participante tiene 18 justos y será evaluado para poder concursar"
     )
   } else if (edad < 18) {
-    throw new edadIlegal("El participante no tiene 18, y por tanto no cumole requisitos")
+    throw new edadIlegal(
+      "El participante no tiene 18, y por tanto no cumole requisitos"
+    )
   }
 }
 
 try {
-    comprobarEdadTorneo(edadParticipante1)
+  comprobarEdadTorneo(edadParticipante1)
 } catch (error) {
-    if (error instanceof edadIlegal) {
-        console.error(error.message)
-    }
+  if (error instanceof edadIlegal) {
+    console.error(error.message)
+  }
 } finally {
-    console.log("FIN DE EJERCICIO 1")
+  console.log("FIN DE EJERCICIO 1")
+  console.log(
+    "Este mensaje muestra info adicional sobre las condiciones\n +18 : Ok\n =18 : A revision\n <18 : Rechazado"
+  )
 }
-
-// 2. Crea una función que utilice warn correctamente
-
-// 3. Crea una función que utilice info correctamente
-
 // 4. Utiliza table
 
+let miTablilla = [
+  { Nombre: "Jonatan", Apellido: "Gomez", Edad: 37, Salario: 24000 },
+  { Nombre: "Sandra", Apellido: "Nieves", Edad: 34, Salario: 18000 },
+  { Nombre: "Liam", Apellido: "Gomez Nieves", Edad: 4, Salario: 0 }
+]
+
+console.table(miTablilla)
+
 // 5. Utiliza group
+console.group("Usuarios")
+console.log("Jon")
+console.log("San")
+console.log("LGN")
+console.groupEnd("Usuarios")
+
+console.log("Users FINALIZADOS")
 
 // 6. Utiliza time
 
